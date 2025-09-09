bac1882f06e22ab04c883c3f5c9103c4933f506b
Alaina Shaw <galainashaw@gmail.com>
2025-09-09 18:21:58 -0400

    created an experiment "mrt.js" for Task Set 2 with 3 questions followed by answer confirmations

---
+++ web/week2/mrt.js
@@ -0,0 +1,21 @@
+let experimentName = 'Stroop';
+
+let welcomeMessage = 'In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.';
+
+console.log(experimentName);
+
+alert(welcomeMessage);
+
+let answer1 = prompt('What is 1+3?');
+
+alert('You answered ' + answer1);
+
+let answer2 = prompt('What is 5+7?');
+
+alert('You answered ' + answer2);
+
+let answer3 = prompt('What is 8+6?');
+
+alert('You answered ' + answer3);
+
+alert('Thank you for your participation!')
