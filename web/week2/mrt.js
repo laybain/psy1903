4c208278f0aba8bf62a57b00a94778bcbd00ee45
Alaina Shaw <galainashaw@gmail.com>
2025-09-11 18:57:06 -0400

    added random number generation to experiment defined question string and later implemented "prompt"

--- web/week2/mrt.js
+++ web/week2/mrt.js
@@ -6,16 +6,35 @@ console.log(experimentName);
 
 alert(welcomeMessage);
 
-let answer1 = prompt('What is 1+3?');
+let num1 = Math.floor(Math.random() * 10) + 1;
+let num2 = Math.floor(Math.random() * 10) + 1;
+let question1 = ('What is ' + num1 + '+' + num2 + '?');
 
-alert('You answered ' + answer1);
+let start1 = Date.now();
+let answer1 = prompt(question1);
+let end1 = Date.now();
+let responseTime1 = ((end1 - start1) / 1000).toFixed(3);
+alert('You answered ' + answer1 + ' in ' + responseTime1 + ' seconds.');
 
-let answer2 = prompt('What is 5+7?');
+let num3 = Math.floor(Math.random() * 10) + 1;
+let num4 = Math.floor(Math.random() * 10) + 1;
 
-alert('You answered ' + answer2);
+let question2 = ('What is ' + num3 + '+' + num4 + '?');
 
-let answer3 = prompt('What is 8+6?');
+let start2 = Date.now();
+let answer2 = prompt(question2);
+let end2 = Date.now();
+let responseTime2 = ((end2 - start2) / 1000).toFixed(3);
+alert('You answered ' + answer2 + ' in ' + responseTime2 + ' seconds.');
 
-alert('You answered ' + answer3);
+let num5 = Math.floor(Math.random() * 10) + 1;
+let num6 = Math.floor(Math.random() * 10) + 1;
 
-alert('Thank you for your participation!')
+let question3 = ('What is ' + num5 + '+' + num6 + '?');
+
+let start3 = Date.now();
+let answer3 = prompt(question2);
+let end3 = Date.now();
+let responseTime3 = ((end3 - start3) / 1000).toFixed(3);
+alert('You answered ' + answer3 + ' in ' + responseTime3 + ' seconds.');
+
