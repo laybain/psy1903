f2fdb487bc8185604f0aa5a9fbaffae0dc3c70fe
Alaina Shaw <galainashaw@gmail.com>
2025-09-16 15:49:02 -0400

    creating week 3 task set directory and html and js files

---
+++ web/week3-task-set/app.js
@@ -0,0 +1,11 @@
+//Identify elements on the page we will update 
+let num1 = document.getElementById('num1')
+let num2 = document.getElementById('num2')
+
+//Generate random numbers to display on page load 
+let randomNum1 = Math.floor(Math.random() * 10) + 1;
+let randomNum2 = Math.floor(Math.random() * 10) + 1;
+
+// Update elements on the page 
+num1.innerHTML = randomNum1;
+num2.innerText = randomNum2;
