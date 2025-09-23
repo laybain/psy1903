704a69b51fec09f1476b28e6d13215e05633c72f
Alaina Shaw <galainashaw@gmail.com>
2025-09-23 17:19:51 -0400

    function named convertTemp that converts a temperature between Celsius and Fahrenheit.

--- web/week4-task-set/app.js
+++ web/week4-task-set/app.js
@@ -1,4 +1,4 @@
-
+/*
 
 function celsiusToFahrenheit(temperature) {
     return (temperature * 1.8) + 32;
@@ -6,3 +6,23 @@ function celsiusToFahrenheit(temperature) {
 
 console.log(celsiusToFahrenheit(10)); // Expected output: 50
 console.log(celsiusToFahrenheit(-5)); // Expected output: 23
+*/
+
+//function c-->f f-->c
+function convertTemp(temperature, convertTo) {
+
+    //conditional determining whether given temp is in c or f
+
+    //if c then (°C * 1.8) + 32 = °F
+    if (convertTo == 'c') {
+        return ((temperature - 32) / 1.8)
+    }
+    //if f then (°F - 32) / 1.8 = °C
+    else {
+        return (temperature * 1.8) + 32;
+    }
+
+}
+
+console.log(convertTemp(10, 'c')); // Expected output: -12.222222222222221
+console.log(convertTemp(10, 'f')); // Expected output: 50
