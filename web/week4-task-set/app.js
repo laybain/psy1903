9ce8c6a81413e1c424f50d969dbca65163210ec6
Alaina Shaw <galainashaw@gmail.com>
2025-09-23 16:53:30 -0400

    created a function that converts temp from celsius to fahrenheit

---
+++ web/week4-task-set/app.js
@@ -0,0 +1,8 @@
+
+
+function celsiusToFahrenheit(temperature) {
+    return (temperature * 1.8) + 32;
+}
+
+console.log(celsiusToFahrenheit(10)); // Expected output: 50
+console.log(celsiusToFahrenheit(-5)); // Expected output: 23
