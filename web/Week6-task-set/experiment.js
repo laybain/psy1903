20fb6794b0b0fb288d90916a81147d7e11052533
Alaina Shaw <galainashaw@gmail.com>
2025-10-07 18:00:16 -0400

    enhance the visual design of your Math Response Time experiment by adding custom CSS.

---
+++ web/Week6-task-set/experiment.js
@@ -0,0 +1,67 @@
+let jsPsych = initJsPsych();
+
+let timeline = [];
+
+//Welcome screen
+let welcome = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: `
+    <h2 class="experiment-heading">Welcome to the Math Response Time Task!</h2>
+  <p>In this experiment, you will be shown a series of math questions.<br>
+  <p>There are three parts to this experiment; the questions will increase in difficulty with each part.<br>
+  Please answer as quickly and accurately as possible.</p>
+  <p>Press <span class='key'>SPACE</span> to begin.</p>
+  `,
+    choices: [' ']
+};
+
+timeline.push(welcome);
+
+//Make trials with a for loop
+let num_trials = 3;
+for (let i = 0; i < num_trials; i++) {
+    // Generate two random numbers 
+    let num1 = Math.floor(Math.random() * 10) + 1;
+    let num2 = Math.floor(Math.random() * 10) + 1;
+    let correctAnswer = num1 + num2;
+
+    let math_trial = {
+        type: jsPsychSurveyHtmlForm,
+        html: `<span class="equation"><span class="eqnum">${num1}</span> + <span class="eqnum">${num2}</span> = 
+<input name="answer" type="text" required /></span>`,
+        data: {
+            num1: num1,
+            num2: num2,
+            correctAnswer: correctAnswer
+        },
+        button_label: "Submit"
+    };
+    timeline.push(math_trial);
+
+}
+
+
+let debriefTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: `
+    <h1>Thank you!</h1>
+    <p>You can now close this tab.</p>
+    `,
+    choices: "NO_KEYS",
+    on_start: function () {
+        let data = jsPsych.data
+            .get()
+            .filter({ collect: true }) //filter({ trial_type: 'survey-html-form' })
+            .ignore(['response', 'stimulus', 'trial_type', 'trial_index', 'plugin_version'])
+            .csv();
+        console.log(data);
+        console.log(jsPsych.data.get().values());
+    }
+};
+timeline.push(debriefTrial);
+
+
+jsPsych.run(timeline);
+
+
+
